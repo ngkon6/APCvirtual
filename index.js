f860bb6b7cd93abafc5eeb82f64833e7c0ba8ead
ILoveAndLikePizza <nathankoning@gmail.com>
2023-06-06 14:28:34 +0200

    Add a color map for the pads

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const easymidi = require("easymidi");
+const path = require("path");
+const { EOL } = require("os");
+const fs = require("fs");
 
 const inputs = easymidi.getInputs();
 const outputs = easymidi.getOutputs();
@@ -10,6 +13,53 @@ for (let i=0; i<outputs.length; i++) console.log(`${i} >> ${outputs[i]}`);
 
 const input = new easymidi.Input(inputs[1]);
 const output = new easymidi.Output(outputs[0]);
+const controller = new easymidi.Output(outputs[1]);
+
+const colors = {
+    black: 0,
+    white: 3,
+    red: 5,
+    warmwhite: 8,
+    orange: 9,
+    yellow: 13,
+    mintgreen: 20,
+    green: 21,
+    seagreen: 25,
+    cyan: 37,
+    lavender: 41,
+    blue: 45,
+    violet: 49,
+    magenta: 53,
+    pink: 57
+};
+
+let note = 0;
+if (fs.existsSync(path.join(__dirname, "colormap.txt"))) {
+    fs.readFile(path.join(__dirname, "colormap.txt"), function(err, data) {
+        if (err) throw err;
+        const colorMap = new TextDecoder().decode(data).split(EOL);
+        colorMap.reverse();
+        if (colorMap.length != 8) {
+            console.error("The colormap should contain 8 lines!");
+            process.exit(1);
+        } else {
+            for (const line of colorMap) {
+                const row = line.split("|");
+                if (row.length != 8) {
+                    console.error("The colormap lines should contain 8 fields!");
+                    process.exit(2);
+                } else {
+                    for (const cell of row) {
+                        if (cell in colors) controller.send("noteon", {note: note, velocity: colors[cell], channel: 0});
+                        else console.warn("The given color was not found in the color scheme.");
+
+                        note++;
+                    }
+                }
+            }
+        }
+    });
+} else console.warn("No colormap.txt was found, so the pads will not be illuminated.");
 
 input.on("noteon", function(res) {
     if (process.argv.includes("-v")) console.log(`Note on: note ${res.note} set to ${res.velocity} in channel ${res.channel}`);
